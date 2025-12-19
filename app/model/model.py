658e8644b40bbb01d1bf5b3d3e5ad8840434468e
Yeongrok Song <song7868@gmail.com>
2025-12-19 19:56:16 +0900

    feat: cosmic modle

--- app/model/model.py
+++ app/model/model.py
@@ -1,19 +1,41 @@
 from dataclasses import dataclass
+from typing import NewType, Optional
 
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
 
-@dataclass(frozen=True) #()
+@dataclass(frozen=True)  #(1) (2)
 class OrderLine:
     orderid: str
     sku: str
     qty: int
-    
+
+
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, Optional[date]):
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):  #(2)
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
-        
-    def allocaate(self, line: Orderline):
+        self._purchased_quantity = qty
+        self._allocations = set() #type: Set[OrderLine]
+
+    def allocate(self, line: OrderLine):  #(3)
         self.available_quantity -= line.qty
-        
+    
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+    
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+    
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+    
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+    
+    
